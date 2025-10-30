baa4a2af1705e5c433b9738b00a0e5f563504387
loongson-jvm <jvm@loongson.cn>
2025-10-30 10:31:09 +0800

    Update (2025.10.30)
    27906: Inline ActiveCoresMP
    30520: UseActiveCoresMP should be able to be turned off on a single-core machine

--- src/hotspot/os/linux/os_linux.hpp
+++ src/hotspot/os/linux/os_linux.hpp
@@ -22,6 +22,12 @@
  *
  */
 
+/*
+ * This file has been modified by Loongson Technology in 2023. These
+ * modifications are Copyright (c) 2023, Loongson Technology, and are made
+ * available on the same license terms set forth above.
+ */
+
 #ifndef OS_LINUX_VM_OS_LINUX_HPP
 #define OS_LINUX_VM_OS_LINUX_HPP
 
@@ -250,6 +256,8 @@ class Linux {
   // Stack repair handling
 
   // none present
+
+  static int sched_active_processor_count();
 
  private:
   static void expand_stack_to(address bottom);
